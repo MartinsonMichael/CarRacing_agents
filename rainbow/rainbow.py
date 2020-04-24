038c2b533667d9ddce6e88b7ea767e2f64ba5a6f
Martinson Michael <mmartinson@yandex.ru>
2020-04-24 12:16:00 +0300

    finally fix episodes max num

--- rainbow/rainbow.py
+++ rainbow/rainbow.py
@@ -75,7 +75,7 @@ class Rainbow:
         # Prioritized Replay
         # Anneal beta from beta0 to 1 throughout training
         update_interval = 4
-        betasteps = self.hyperparameters['num_steps_to_run'] / update_interval
+        betasteps = self.config.env_steps_to_run / update_interval
         rbuf = replay_buffer.PrioritizedReplayBuffer(
             10 ** 6, alpha=0.5, beta0=0.4, betasteps=betasteps,
             num_steps=3,
@@ -124,11 +124,12 @@ class Rainbow:
         print('Start to train Rainbow')
         if self.hyperparameters['use_parallel_envs']:
             print('Rainbow  batch+env multiprocessing training start')
-            self._batch_train()
+            raise NotImplemented
+            # self._batch_train()
         else:
             try:
                 print('Rainbow single env training')
-                print(f"num episodes to run : {self.hyperparameters['num_episodes_to_run']}")
+                # print(f"num episodes to run : {self.hyperparameters['num_episodes_to_run']}")
                 while True:
 
                     self.run_one_episode()
@@ -156,103 +157,103 @@ class Rainbow:
                 # self.save(suffix='final')
                 pass
 
-    def _batch_train(self) -> None:
-        raise NotImplemented
-        num_env = self.hyperparameters['parallel_env_num']
-        total_reward = np.zeros(num_env, dtype=np.float32)
-        episode_len = np.zeros(num_env, dtype=np.int32)
-        state = self.env.reset()
-        print('start loop')
-        while True:
-            self.global_step_number += num_env
-            self.batch_step_number += 1
-
-            actions = self.agent.batch_act_and_train(state)
-            state, reward, dones, infos = self.env.step(actions)
-
-            # print('state')
-            # print(state)
-            # print(type(state), state.shape)
-            # print(type(state[0]), state[0].shape)
-            # print(type(state[0][0]), state[0][0].shape)
-            # print(type(state[0][1]), state[0][1].shape)
-            #
-            # exit(1)
-
-            print(f"make action, end : {np.sum(dones)}")
-
-            total_reward += reward
-            episode_len += 1
-
-            # Compute mask for done and reset
-            resets = np.logical_or(
-                episode_len >= self.hyperparameters['max_episode_len'],
-                np.logical_or(
-                    [info.get('needs_reset', False) for info in infos],
-                    [info.get('need_reset', False) for info in infos],
-                ),
-            )
-
-            print('make batch train')
-            # Agent observes the consequences
-            self.agent.batch_observe_and_train(state, reward, dones, resets)
-
-            print('update stats')
-            self.update_current_game_stats(reward[0], dones[0], infos[0])
-
-            if self.global_step_number > self.hyperparameters['num_steps_to_run']:
-                print("exit due to 'num_steps_to_run'")
-                break
-
-            # if self.global_step_number % self.hyperparameters['save_frequency_episode'] == 0:
-            #     self.save()
-
-            end = np.logical_or(resets, dones)
-
-            # print(f'reset : {resets}')
-            # print(f'dones : {dones}')
-            # print(f'end : {end}')
-
-            if self.batch_step_number > 100:
-                break
-
-            if end[0]:
-                print('log 0')
-                self._exp_moving_track_progress = (
-                        0.98 * self._exp_moving_track_progress +
-                        0.02 * self.current_game_stats.get('track_progress', 0)
-                )
-                self.current_game_stats.update({
-                    'moving_track_progress': self._exp_moving_track_progress,
-                    'total_env_episode': self.episode_number,
-                    'total_grad_steps': self._total_grad_steps,
-                })
-                self.stat_logger.log_it(self.current_game_stats)
-                if self._exp_moving_track_progress >= \
-                        self.hyperparameters.get('track_progress_success_threshold', 10):
-                    break
-                self.flush_stats()
-
-            self.episode_number += int(end.sum())
-
-            total_reward[end] = 0
-            episode_len[end] = 0
-
-            if np.any(end):
-                print('any end')
-
-                print('end : ', end)
-                print('reward : ', total_reward)
-                print('len : ', episode_len)
-
-                state = self.env.reset(np.arange(num_env)[end])
-
-            if self.batch_step_number % self.config.animation_record_frequency == 0:
-                self._run_eval_episode()
-
-        # finally:
-        #     self.stat_logger.on_training_end()
-        #     self.save(suffix='final')
+    # def _batch_train(self) -> None:
+    #     raise NotImplemented
+    #     num_env = self.hyperparameters['parallel_env_num']
+    #     total_reward = np.zeros(num_env, dtype=np.float32)
+    #     episode_len = np.zeros(num_env, dtype=np.int32)
+    #     state = self.env.reset()
+    #     print('start loop')
+    #     while True:
+    #         self.global_step_number += num_env
+    #         self.batch_step_number += 1
+    #
+    #         actions = self.agent.batch_act_and_train(state)
+    #         state, reward, dones, infos = self.env.step(actions)
+    #
+    #         # print('state')
+    #         # print(state)
+    #         # print(type(state), state.shape)
+    #         # print(type(state[0]), state[0].shape)
+    #         # print(type(state[0][0]), state[0][0].shape)
+    #         # print(type(state[0][1]), state[0][1].shape)
+    #         #
+    #         # exit(1)
+    #
+    #         print(f"make action, end : {np.sum(dones)}")
+    #
+    #         total_reward += reward
+    #         episode_len += 1
+    #
+    #         # Compute mask for done and reset
+    #         resets = np.logical_or(
+    #             episode_len >= self.hyperparameters['max_episode_len'],
+    #             np.logical_or(
+    #                 [info.get('needs_reset', False) for info in infos],
+    #                 [info.get('need_reset', False) for info in infos],
+    #             ),
+    #         )
+    #
+    #         print('make batch train')
+    #         # Agent observes the consequences
+    #         self.agent.batch_observe_and_train(state, reward, dones, resets)
+    #
+    #         print('update stats')
+    #         self.update_current_game_stats(reward[0], dones[0], infos[0])
+    #
+    #         if self.global_step_number > self.hyperparameters['num_steps_to_run']:
+    #             print("exit due to 'num_steps_to_run'")
+    #             break
+    #
+    #         # if self.global_step_number % self.hyperparameters['save_frequency_episode'] == 0:
+    #         #     self.save()
+    #
+    #         end = np.logical_or(resets, dones)
+    #
+    #         # print(f'reset : {resets}')
+    #         # print(f'dones : {dones}')
+    #         # print(f'end : {end}')
+    #
+    #         if self.batch_step_number > 100:
+    #             break
+    #
+    #         if end[0]:
+    #             print('log 0')
+    #             self._exp_moving_track_progress = (
+    #                     0.98 * self._exp_moving_track_progress +
+    #                     0.02 * self.current_game_stats.get('track_progress', 0)
+    #             )
+    #             self.current_game_stats.update({
+    #                 'moving_track_progress': self._exp_moving_track_progress,
+    #                 'total_env_episode': self.episode_number,
+    #                 'total_grad_steps': self._total_grad_steps,
+    #             })
+    #             self.stat_logger.log_it(self.current_game_stats)
+    #             if self._exp_moving_track_progress >= \
+    #                     self.hyperparameters.get('track_progress_success_threshold', 10):
+    #                 break
+    #             self.flush_stats()
+    #
+    #         self.episode_number += int(end.sum())
+    #
+    #         total_reward[end] = 0
+    #         episode_len[end] = 0
+    #
+    #         if np.any(end):
+    #             print('any end')
+    #
+    #             print('end : ', end)
+    #             print('reward : ', total_reward)
+    #             print('len : ', episode_len)
+    #
+    #             state = self.env.reset(np.arange(num_env)[end])
+    #
+    #         if self.batch_step_number % self.config.animation_record_frequency == 0:
+    #             self._run_eval_episode()
+    #
+    #     # finally:
+    #     #     self.stat_logger.on_training_end()
+    #     #     self.save(suffix='final')
 
     def save(self, suffix=None):
         pass
@@ -310,46 +311,46 @@ class Rainbow:
                     print('Was made panic env reset...')
                     raise ValueError
                 break
-
-    def _run_eval_episode(self):
-        print('run eval episode')
-        # record_anim = (
-        #         self.batch_step_number % self.hyperparameters.get('animation_record_frequency', 1e6) == 0 and
-        #         self.hyperparameters.get('record_animation', False)
-        # )
-        record_anim = True
-
-        done = False
-        self.episode_number += 1
-        total_reward = 0
-        episode_len = 0
-        images = []
-
-        state = self.test_env.reset()
-
-        while not done:
-
-            action = self.agent.act(state)
-            next_state, reward, done, info = self.test_env.step(action)
-
-            if record_anim:
-                images.append(self.test_env.render(full_image=True))
-
-            total_reward += reward
-            episode_len += 1
-
-            if done \
-                    or info.get('need_reset', False) \
-                    or episode_len > self.hyperparameters['max_episode_len'] \
-                    or info.get('needs_reset', False):
-
-                if record_anim:
-                    Process(
-                        target=save_as_mp4,
-                        args=(
-                            images,
-                            f'animation/Rainbow/{self.name}/_R:_{total_reward}_Time:_{episode_len}_{time.time()}.mp4',
-                            self.stat_logger
-                        ),
-                    ).start()
-                break
+    #
+    # def _run_eval_episode(self):
+    #     print('run eval episode')
+    #     # record_anim = (
+    #     #         self.batch_step_number % self.hyperparameters.get('animation_record_frequency', 1e6) == 0 and
+    #     #         self.hyperparameters.get('record_animation', False)
+    #     # )
+    #     record_anim = True
+    #
+    #     done = False
+    #     self.episode_number += 1
+    #     total_reward = 0
+    #     episode_len = 0
+    #     images = []
+    #
+    #     state = self.test_env.reset()
+    #
+    #     while not done:
+    #
+    #         action = self.agent.act(state)
+    #         next_state, reward, done, info = self.test_env.step(action)
+    #
+    #         if record_anim:
+    #             images.append(self.test_env.render(full_image=True))
+    #
+    #         total_reward += reward
+    #         episode_len += 1
+    #
+    #         if done \
+    #                 or info.get('need_reset', False) \
+    #                 or episode_len > self.hyperparameters['max_episode_len'] \
+    #                 or info.get('needs_reset', False):
+    #
+    #             if record_anim:
+    #                 Process(
+    #                     target=save_as_mp4,
+    #                     args=(
+    #                         images,
+    #                         f'animation/Rainbow/{self.name}/_R:_{total_reward}_Time:_{episode_len}_{time.time()}.mp4',
+    #                         self.stat_logger
+    #                     ),
+    #                 ).start()
+    #             break
