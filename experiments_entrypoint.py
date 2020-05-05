89c5db90b578e6f9d05c8e38469b71b640e31fdd
Martinson Michael <mmartinson@yandex.ru>
2020-05-05 15:48:16 +0300

    fix

--- experiments_entrypoint.py
+++ experiments_entrypoint.py
@@ -137,7 +137,7 @@ def launch(exp_config: Dict[str, Any]) -> None:
     wandb.init(
         project='CarRacing_MassExp' + ('_test' if 'test' in exp_config['exp_series_name'] else ''),
         reinit=True,
-        entity=WANDB_ENTITY,
+        entity=WANDB_ENTITY if WANDB_ENTITY == 'cds' else None,
         name=launch_id,
         notes=launch_note,
         config={
