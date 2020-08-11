13efb301dd47828eb45a7d98d828e5d98e611577
Martinson Michael <mmartinson@yandex.ru>
2020-08-11 14:18:53 +0300

    fix script for rnd launching

--- run_rnd_exp.sh
+++ run_rnd_exp.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
-if [[ $# -ne 2]]; then
-	echo 'please pass 2 arguments, DEVICE and NAME'
+if [[ $# != 2 ]]; then
+	echo "please pass 2 arguments, DEVICE and NAME"
 	exit 0
 fi
 
 wandb_key=$(cat WANDB_API_KEY.txt) 
 
-echo $wandb_key
-
 docker run -it \
 	-v .:/src \
 	-e WANDB_API_KEY=$wandb_key \
 	-e DEVICE=$1 \
-        -e NAME=$2 \	
-	car-racing bash \
+        -e NAME=$2 \
+	car-racing:latest bash \
 		cd /src/rnd_continues &&
 		python train.py
