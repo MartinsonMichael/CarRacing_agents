18d617850462a9c9ff69092e73632342ff1edd4a
Martinson Michael <mmartinson@yandex.ru>
2020-08-13 13:51:28 +0300

    some rnd fixes

--- run_rnd_exp.sh
+++ run_rnd_exp.sh
@@ -7,13 +7,34 @@ fi
 
 wandb_key=$(cat WANDB_API_KEY.txt) 
 
-docker run -it \
-	-v "$PWD":/src \
-	-e WANDB_API_KEY=$wandb_key \
-	-e DEVICE=$1 \
-        -e NAME=$2 \
-	--gpus all \
-	car-racing:latest bash -c \
-	  "echo 'Use python3.6 version:' && \
-	  python3.6 --version && \
-	  cd rnd_continues && python3.6 train.py"
+if [[ $1 = 'cpu' ]]; then
+  echo "Run CPU version of RND"
+
+  docker run -it \
+    -v "$PWD":/src \
+    -e WANDB_API_KEY=$wandb_key \
+    -e DEVICE=$1 \
+    -e NAME=$2 \
+    car-racing:latest bash -c \
+      "echo 'Use python3.6 version:' && \
+      python3.6 --version && \
+      cd rnd_continues && python3.6 train.py"
+else
+  echo "Run GPU version of RND"
+
+  docker run -it \
+    --gpus all \
+    -v "$PWD":/src \
+    -e WANDB_API_KEY=$wandb_key \
+    -e DEVICE=$1 \
+    -e NAME=$2 \
+    car-racing:latest bash -c \
+      "echo 'Use python3.6 version:' && \
+      python3.6 --version && \
+      echo 'User' $USER && \
+      ls -lah && ls -lah drq && \
+      cd rnd_continues && python3.6 train.py"
+fi
+
+
+
